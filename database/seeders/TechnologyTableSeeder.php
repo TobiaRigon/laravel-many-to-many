f90f39761df31e1291dda6c5910e54bb4530a5de
TobiaRigon <rigontobia@gmail.com>
2024-02-25 10:29:39 +0100

    Create: technology model T factory + TSeeder

---
+++ database/seeders/TechnologyTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Technology;
+use App\Models\Project;
+
+class TechnologyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Technology :: factory()
+                    ->count(7)
+                    ->create()
+                    ->each(function($technology){
+                        $projects = Project :: inRandomOrder()-> limit(3)->get();
+
+                        // dd($projects);
+                    });
+    }
+}
