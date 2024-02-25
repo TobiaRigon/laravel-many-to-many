70aed0c85710e64e769f23182d03d9bd427165da
TobiaRigon <rigontobia@gmail.com>
2024-02-25 09:54:15 +0100

    Feat: FK migration project_technology drop

--- database/migrations/9999_02_23_085807_add_foreign_keys.php
+++ database/migrations/9999_02_23_085807_add_foreign_keys.php
@@ -39,5 +39,17 @@ return new class extends Migration
 
             $table -> dropColumn('type_id');
         });
+
+Schema::table('project_technology', function (Blueprint $table) {
+
+            $table -> dropForeign('project_technology_project_id_foreign');
+            $table -> dropColumn('project_id');
+
+            $table -> dropForeign('project_technology_technology_id_foreign');
+            $table -> dropColumn('technology_id');
+
+        });
+
+
     }
 };
